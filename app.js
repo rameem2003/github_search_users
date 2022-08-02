8d9138f02ffee67bd667b8c6f47f0abe62e3a265
rameem2003 <rameem2019@gmail.com>
2022-08-02 22:19:02 +0600

    responsive update

--- app.js
+++ app.js
@@ -8,21 +8,27 @@ search.addEventListener("click", () => {
     console.log(user_name);
 
 
-    // profile ui
-    document.getElementById("profile_ui").classList.add("active");
+    if(user_name != ""){
+        // profile ui
+        document.getElementById("profile_ui").classList.add("active");
 
-    fetch(`https://api.github.com/users/${user_name}`)
-    .then(response => response.json().then(data=> {
-        console.log(data);
+        fetch(`https://api.github.com/users/${user_name}`)
+        .then(response => response.json().then(data=> {
+            console.log(data);
 
-        // all data fields
-        document.getElementById("github_name").innerHTML = data.name;
-        document.getElementById("bio").innerHTML = data.bio;
-        document.getElementById("location").innerHTML = `<i class="fa-solid fa-location-arrow"></i> ${data.location}`;
-        document.getElementById("followers").innerHTML = `${data.followers} Follwers`;
-        document.getElementById("following").innerHTML = `${data.following} Following`;
-        document.getElementById("avatar").innerHTML = `<img src="${data.avatar_url}" alt="">`;
-    }))
+            // all data fields
+            document.getElementById("github_name").innerHTML = data.name;
+            document.getElementById("bio").innerHTML = data.bio;
+            document.getElementById("location").innerHTML = `<i class="fa-solid fa-location-arrow"></i> ${data.location}`;
+            document.getElementById("followers").innerHTML = `${data.followers} Follwers`;
+            document.getElementById("following").innerHTML = `${data.following} Following`;
+            document.getElementById("avatar").innerHTML = `<img src="${data.avatar_url}" alt="">`;
+        }))
+    }else{
+        alert("Please Enter github username!!!");
+    }
+
+    
 
 
     
