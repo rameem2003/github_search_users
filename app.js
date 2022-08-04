fbf11177f6cfbd584b37851614ebdf039ce99f7e
rameem2003 <rameem2019@gmail.com>
2022-08-04 08:11:41 +0600

    update

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const search = document.getElementById("search");
 
 
 search.addEventListener("click", () => {
+    const wrapper = document.getElementById("wrapper");
     const user_name = document.getElementById("user_name").value;
     console.log(user_name);
 
 
     if(user_name != ""){
+        // wrapper
+        wrapper.style.display = "none";
         // profile ui
         document.getElementById("profile_ui").classList.add("active");
 
@@ -22,6 +25,12 @@ search.addEventListener("click", () => {
             document.getElementById("location").innerHTML = `<i class="fa-solid fa-location-arrow"></i> ${data.location}`;
             document.getElementById("followers").innerHTML = `${data.followers} Follwers`;
             document.getElementById("following").innerHTML = `${data.following} Following`;
+            document.getElementById("repo").innerHTML = `${data.public_repos} Repositories`;
+            document.getElementById("link").innerHTML = `
+                <a class="link" href="https://${data.blog}" target="_blank"><i class="fa-solid fa-globe"></i></a>
+                <a class="link" href="${data.html_url}" target="_blank"><i class="fa-brands fa-github"></i></a>
+                <a class="link" href="https://twitter.com/${data.twitter_username}" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+            `
             document.getElementById("avatar").innerHTML = `<img src="${data.avatar_url}" alt="">`;
         }))
     }else{
